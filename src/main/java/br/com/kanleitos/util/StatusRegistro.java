ced0a9b4c4eb520d7cfbe04ddfe69e83c35122ea
pongelupe <pongelupe1998@gmail.com>
2018-04-02 08:21:56 -0300

    adicionado

--- src/main/java/br/com/kanleitos/util/StatusRegistro.java
+++ src/main/java/br/com/kanleitos/util/StatusRegistro.java
@@ -1,7 +1,7 @@
 package br.com.kanleitos.util;
 
 public enum StatusRegistro {
-	EM_ANDAMENTO("Em Andamento"), ALTA("Alta"), OBITO("Óbito"), TRANSFERENCIA("Transferência");
+	EM_ANDAMENTO("Em Andamento"), ALTA("Alta"), OBITO("Óbito"), TRANSFERENCIA("Transferência"), PAD("PAD");
 
 	private String nome;
 
@@ -23,6 +23,8 @@ public enum StatusRegistro {
 			return StatusRegistro.OBITO;
 		case "Transferência":
 			return StatusRegistro.TRANSFERENCIA;
+		case "PAD":
+			return StatusRegistro.PAD;
 		default:
 			throw new IllegalArgumentException("Unknown" + statusRegistro);
 		}
